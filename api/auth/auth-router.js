5dc32ea762559be8af18b4a25803c4834bbc719f
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-02-23 09:51:32 -0500

    updated

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,25 @@
 const express = require("express")
+const { userParams } = require("../../data/db-config.js")
 const router = express.Router()
-const User = require("../users/users-model.js")
+const Users = require("../users/users-model.js")
 // const bcrypt = require("bcryptjs")
 
+router.post("/register", (req, res) => {
+  res.json('silly register')
+  // Users.insert(req.body)
+  // .then(200).json({})
+})
+module.exports = router;
+
+router.post("/login", (req, res) => {
+    res.json('silly login')
+
+  // Users.insert(req.body)
+  //   .then(200).json()
+})
+
+router.get("/logout", (req, res) => {
+    res.json('silly logout')
+
+  // Users.findById(req.params.id)
+})
